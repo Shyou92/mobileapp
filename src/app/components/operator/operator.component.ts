e9b6207af7013748a4a5f7fb1232806ad6b969a5
Shyou_92 <vitalex92@yandex.ru>
2022-02-27 00:05:58 +0300

    form with validation

--- src/app/components/operator/operator.component.ts
+++ src/app/components/operator/operator.component.ts
@@ -1,5 +1,5 @@
-import { Component, Injectable, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Services } from 'app/services';
@@ -28,15 +28,11 @@ export class OperatorComponent {
     amountOfMoney: '',
   });
 
-  // data = new FormGroup({
-  //   phoneNumber: new FormControl(''),
-  //   amountOfMoney: new FormControl(''),
-  // });
-
   constructor(
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
     private services: Services,
+    private router: Router,
     public snackBar: MatSnackBar
   ) {
     this.cards = cards;
@@ -46,18 +42,38 @@ export class OperatorComponent {
 
   getSingleCardForm(id: number) {
     const card = this.cards.filter((item) => item.id === id);
-    return card ? card[0] : card[0];
+    return card[0];
   }
 
   submitForm() {
-    // console.log(this.checkoutForm.value);
     const data = this.checkoutForm.value;
-    this.services.sendFormData(data).then((res) =>
-      this.snackBar.openFromComponent(SnackbarComponent, {
-        duration: 5000,
-        data: `${res.data.message}: ${res.data.sent.amountOfMoney}₽ for ${res.data.sent.phoneNumber}.
-       You will be redirected to main page in 5 seconds`,
+    this.services
+      .sendFormData(data, this.card)
+      .then((res) => {
+        if (res.status === 200) {
+          this.snackBar.openFromComponent(SnackbarComponent, {
+            duration: 5000,
+            data: `${res.data.message}: ${res.data.sent.amountOfMoney}₽ for ${res.data.sent.phoneNumber}.
+           You will be redirected to main page in 5 seconds`,
+          });
+          this.checkoutForm.reset();
+          setTimeout(() => {
+            this.router.navigate(['/']);
+          }, 5000);
+        } else {
+          this.snackBar.openFromComponent(SnackbarComponent, {
+            duration: 5000,
+            data: `${res.data.message}: ${res.data.sent.amountOfMoney}₽ for ${res.data.sent.phoneNumber}.
+             Try again one more time.`,
+          });
+        }
       })
-    );
+      .catch((err) => {
+        this.snackBar.openFromComponent(SnackbarComponent, {
+          duration: 5000,
+          data: `${err.data.message}: ${err.data.sent.amountOfMoney}₽ for ${err.data.sent.phoneNumber}.
+         Try again one more time.`,
+        });
+      });
   }
 }
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
-import { FormData, MockFillType } from 'app/typings';
+import { Card, FormData, MockFillType } from 'app/typings';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Services {
-  public sendFormData(data: FormData): Promise<MockFillType> {
-    const randomNumber = Math.floor(Math.random() * (10 - 1)) + 1;
+  private isValid: Boolean;
 
+  constructor() {
+    this.isValid = false;
+  }
+
+  public sendFormData(data: FormData, card: Card): Promise<MockFillType> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (randomNumber > 2) {
+        if (this.formItemValidate(data, card, this.isValid)) {
+          this.isValid = true;
           resolve({
             status: 200,
             success: true,
             data: { sent: data, message: 'Payment is successful' },
           });
         } else {
+          this.isValid = false;
           reject({
             status: 500,
             success: false,
@@ -24,6 +30,20 @@ export class Services {
           });
         }
       }, 2000);
+    });
+  }
+
+  private formItemValidate(data: any, card: Card, isValid: Boolean) {
+    const { codes } = card;
+
+    const { phoneNumber } = data;
+    const phoneNumberSubstr = phoneNumber.slice(1, 5);
+    return codes.find((item: any) => {
+      if (phoneNumberSubstr.includes(item.toString())) {
+        return true;
+      } else {
+        return false;
+      }
     });
   }
 }
