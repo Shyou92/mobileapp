971a1c5c39731528d7d16e87f30dcb34aa121c33
Shyou_92 <vitalex92@yandex.ru>
2022-03-02 15:11:49 +0300

    fix compilation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,7 +65,6 @@ export const routes: Routes = [
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    MatToolbarModule,
     ReactiveFormsModule,
     RouterModule.forRoot(routes),
   ],
